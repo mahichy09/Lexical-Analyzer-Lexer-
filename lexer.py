bcb3aeaeddbf7726149d915b2dc8241afc046456
mahichy09 <s.mahichowdhury09@gmail.com>
2026-04-10 14:39:32 +0600

    Add support for 1D vector pattern in lexer

--- lexer.py
+++ lexer.py
@@ -12,7 +12,7 @@ patterns = [
     ("Library",              r'#include\s*[<"]([^>"]+)[>"]'),
     ("Single Line Comment",  r'//[^\n]*'),
     ("Multi Line Comment",   r'/\*[\s\S]*?\*/'),
-    ("Character", r"'[^'\\]'|'\\.'"), #'a','1','\n'..... , .- any one(\n , \t...)
+    ("Character",            r"'[^'\\]'|'\\.'"),
     ("String",               r'"[^"]*"'),
     ("Number",               r'[0-9]+(\.[0-9]+)?'),
     ("Relational Operator",  r'==|!=|<=|>=|<|>'),
@@ -21,6 +21,7 @@ patterns = [
     ("Arithmetic Operator",  r'\+\+|--|[+\-*/%]'),
     ("Symbol",               r'[(){}\[\],;:]'),
     ("Word",                 r'[a-zA-Z_][a-zA-Z0-9_]*'),
+    ("1D Vector", r'vector\s*<\s*\w+\s*>\s*\w+\s*(\([^)]*\)|\{[^}]*\}|=\s*\{[^}]*\})?'),
 ]
 
 with open("input.txt", "r") as f:
@@ -29,35 +30,32 @@ with open("input.txt", "r") as f:
 with open("output.txt", "w") as fout:
     i = 0
     while i < len(code):
-
-        # skip whitespace
         if code[i] in ' \t\n':
             i += 1
             continue
 
         matched = False
-
         for token_type, pattern in patterns:
-            m = re.match(pattern, code[i:]) #p by p. 
+            m = re.match(pattern, code[i:])
             if m:
-                word = m.group(0) #Entire matched text - '42' , 'int' .....
-
+                word = m.group(0)
                 if token_type == "Library":
                     fout.write(f"Library : {m.group(1)}\n")
-
                 elif token_type == "Word":
                     if word in keywords:
                         fout.write(f"Keyword : {word}\n")
                     else:
                         rest = code[i + len(word):].lstrip()
-                        if rest and rest[0] == '(':   #func.
-                            fout.write(f"Function : {word}\n") 
+                        if rest and rest[0] == '(':
+                            end = code.index(')', i) + 1
+                            fout.write(f"Function : {code[i:end]}\n")
+                            i = end
+                            matched = True
+                            continue
                         else:
                             fout.write(f"Identifier : {word}\n")
-
                 else:
                     fout.write(f"{token_type} : {word}\n")
-
                 i += len(word)
                 matched = True
                 break
@@ -66,4 +64,4 @@ with open("output.txt", "w") as fout:
             fout.write(f"Invalid Token : {code[i]}\n")
             i += 1
 
-print("Done! Check output.txt")
+print("Check output.txt")
